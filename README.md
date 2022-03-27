4ed32f22f437bb8b316a4f532fa27b1403aeb7db
hyun <coderhyun476@gmail.com>
2022-03-27 21:36:29 +0900

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
+[![Cron Crawling](https://github.com/koreanthinker/billboard-json/actions/workflows/cron-crawling.yml/badge.svg)](https://github.com/koreanthinker/billboard-json/actions/workflows/cron-crawling.yml)
+![Hits](https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Fkrtk-dev%2Fbillboard-player&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false)
+![Stars](https://img.shields.io/github/stars/krtk-dev/billboard-player?style=social)
+
 # billboard-json
 Get json type billboard hot 100 chart
+
+https://raw.githubusercontent.com/KoreanThinker/billboard-json/main/data/recent.json
---
+++ crawler/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['jest.config.js', 'coverage/**/*'],
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/no-unresolved': 0,
+    'max-len': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'require-jsdoc': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'no-invalid-this': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    camelcase: 'off',
+    'new-cap': 'off',
+  },
+};
